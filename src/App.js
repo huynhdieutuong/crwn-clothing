cb53b114ee8bfc4d9dd4d072bfae59e03a9588a7
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2020-10-25 12:24:49 +0700

    Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css';
+import './App.scss';
+import Header from './components/Header';
 import HomePage from './pages/HomePage';
 import ShopPage from './pages/ShopPage';
 
 function App() {
   return (
     <Router>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
